e2edf54807c40a92fc9205df1fea1a903bf213f5
dariiadrozd <darriiadrozd@gmail.com>
2023-08-10 23:56:02 +0200

    start

--- 0.22.Error/8/app.js
+++ 0.22.Error/8/app.js
@@ -1,29 +1,4 @@
-// На входе строка в виде пароля. 
-// Необходимо написать функцию на проверку, что пароль является надежным 
-// (содержит хотя бы 1 букву в большом регистре, числа, буквы, символ, длина не менее 8 символов)
-
-const str = "Darya_2004";
-
-function doPassword(str) {
-    if (str.length < 8) return "недостаточно символов";
-    let countNum = 0;
-    let countToUpperCase = 0;
-    let countToLowerCase = 0;
-    for (let i = 0; i <= str.length; i++) {
-        if (!isNaN(str[i])) {
-            countNum++
-        } else {
-            str[i].toUpperCase() === str[i] ? countToUpperCase++ : countToLowerCase++;
-        }
-    }
-    if (str.includes('@') || str.includes('-') || str.includes('$') || str.includes('*')) {
-        countSymbol++;
-    }
-    if (countNum === 0 || countToUpperCase === 0 || countToLowerCase == 0 || countSymbol === 0) {
-        return 'ненадежный пароль'
-    } else {
-        return 'надежный пароль'
-    };
-}
-let result = doPassword(str)
-console.log(result);
+// 8. Реализуйте функцию, которая принимает массив последовательных
+// (возрастающих) букв и возвращает отсутствующую в массиве. Добавить проверки
+// ["a","b","c","d","f"] -> "e"
+// ["O","Q","R","S"] -> "P"
