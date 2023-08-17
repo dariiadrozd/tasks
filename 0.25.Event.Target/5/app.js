005282027bdd74f51416928ba612154938d904ee
dariiadrozd <darriiadrozd@gmail.com>
2023-08-17 19:58:35 +0200

    start

---
+++ 0.25.Event.Target/5/app.js
@@ -0,0 +1,8 @@
+// Необходимо отобразить кнопку с надписью «Нажми на меня». 
+// По клику на нее вывести alert с сообщением
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click",function(){
+    alert("hi")
+})
