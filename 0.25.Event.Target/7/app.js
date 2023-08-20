8e739ec02823ae9568ca3ca05892b18877f83329
dariiadrozd <darriiadrozd@gmail.com>
2023-08-20 10:16:11 +0200

    start

--- 0.25.Event.Target/7/app.js
+++ 0.25.Event.Target/7/app.js
@@ -1,8 +1,25 @@
-// Необходимо отобразить кнопку с надписью «Нажми на меня». 
-// По клику на нее вывести alert с сообщением
+// *Дана таблица c 1 колонкой. Под таблицей сделайте форму (инпут и кнопка), 
+// с помощью которой можно будет добавить нового значение в таблицу
 
-const btn = document.querySelector("button");
+const btn = document.querySelector('.btn');
+const inp = document.querySelector('.inp');
+const table = document.querySelector('.table');
 
-btn.addEventListener("click",function(){
-    alert("hi")
-})
+btn.addEventListener('click', function () {
+    const inputValue = inp.value; // Это то, что пользователь ввёл в инпут.
+
+    if (inputValue !== '') {  // не пустое ли это значение,
+
+        // Если это не пустая строка:
+        // Вы создаете новую строку (<tr>) 
+        // для таблицы и новую ячейку (<td>) внутри этой строки.
+
+        const newRow = document.createElement('tr');
+        const newCell = document.createElement('td');
+        newCell.textContent = inputValue; // добавляем текстовое содержимое в новую ячейку
+        newRow.appendChild(newCell); // добавляем новую ячейку в новую строку
+        table.appendChild(newRow); // добавляем новую строку в тело таблицы
+
+        inp.value = ''; // очищаем поле ввода, устанавливая его значение в пустую строку
+    }
+});
