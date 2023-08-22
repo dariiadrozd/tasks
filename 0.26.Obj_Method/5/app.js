7ca4ee8cafea05daafa53306177ed0c4e5642c31
dariiadrozd <darriiadrozd@gmail.com>
2023-08-22 19:59:19 +0200

    start

--- 0.26.Obj_Method/5/app.js
+++ 0.26.Obj_Method/5/app.js
@@ -1,10 +1,27 @@
-// На странице расположен маркированный список. 
-// Пользователь кликает на каждый item списка. 
-// Необъодимо отловить на какой из элементов нажал пользователь и отобразить
+// На входе объект и число n, символизирующее количество пар ключ-значение. 
+// Ключи и значения - автоинкремент (генерируется автоматически от 1 до n). 
+// Проверить есть ли ключ 10 в объекте.
 
-const ul = document.querySelector('ul');
 
-ul.addEventListener("click", function (event) {
-    const div = document.querySelector('div');
-    div.innerHTML = event.target.textContent;
-})
+const n = 15;
+
+function doObject(n) {
+    const obj = {};
+    for(let i=0;i<n;i++){
+        obj[i]=i;
+    }
+    return obj;
+}
+
+function CheckKey(obj){
+    const ArrayOfKeys = Object.keys(obj);
+    const key = ArrayOfKeys.some(function(el){
+if(el==10) return true;
+    });
+    return key;
+}
+
+const res = doObject(n);
+const result = CheckKey(res);
+console.log(result);
+
