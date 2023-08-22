a2ca68b53e10219217b9fe447484501e2f690293
dariiadrozd <darriiadrozd@gmail.com>
2023-08-22 20:13:10 +0200

    start

--- 0.26.Obj_Method/6/app.js
+++ 0.26.Obj_Method/6/app.js
@@ -1,10 +1,25 @@
-// На странице расположен маркированный список. 
-// Пользователь кликает на каждый item списка. 
-// Необъодимо отловить на какой из элементов нажал пользователь и отобразить
+// На входе объект и число n, символизирующее количество пар ключ-значение. 
+// Ключи - автоинкремент (генерируется автоматически от 1 до n).
+//  Значение каждого ключа – рандомное число от 1 до 100. 
+//  Вывести каждое значение объекта с помощью values
 
-const ul = document.querySelector('ul');
+const n = 10;
 
-ul.addEventListener("click", function (event) {
-    const div = document.querySelector('div');
-    div.innerHTML = event.target.textContent;
-})
+function doObject(n) {
+    const obj = {};
+    for (let i = 0; i < n; i++) {
+        obj[i] = Math.floor(Math.random() * 100);
+    }
+    return obj;
+}
+
+const obj = doObject(n);
+
+function result(obj) {
+    const arr = Object.values(obj);
+    for (let i = 0; i < arr.length; i++) {
+        console.log(i);
+    }
+}
+
+const res = result(obj);
