fdd45efd9a6406835f27f1429bf7b5c8faeef3ea
dariiadrozd <darriiadrozd@gmail.com>
2023-08-23 23:55:03 +0200

    start

--- 0.26.Obj_Method/7/app.js
+++ 0.26.Obj_Method/7/app.js
@@ -1,2 +1,16 @@
 // На вход подается число. Необходимо каждое значение возвести в степень индекса и вычислить сумму.
 // 12345 = 10+21+32+43+54
+
+function calculatePowerSum(number) {
+    if (typeof number !== 'number' || number <= 0) {
+        throw new Error('Input should be a positive number');
+    }
+    const digits = number.toString().split('').map(Number); // Разбиваем число на цифры
+    let sum = 0;
+
+    for (let i = 0; i < digits.length; i++) {
+        sum += digits[i] ** (i + 1); // Возводим цифру в степень индекса и добавляем к сумме
+    }
+
+    return sum;
+}
