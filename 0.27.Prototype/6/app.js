3a384f537434baa32f22874a8998e29da42e8e16
dariiadrozd <darriiadrozd@gmail.com>
2023-08-26 13:39:51 +0200

    start

--- 0.27.Prototype/6/app.js
+++ 0.27.Prototype/6/app.js
@@ -1,24 +1,18 @@
-// На вход подается число. Необходимо каждое значение возвести в степень индекса и вычислить сумму.
-// 12345 = 10+21+32+43+54
+// На входе объект. Ключи и значения - автоинкремент 
+// (генерируется автоматически от 1 до n). 
+// Необходимо отобразить на экране объект 
+// (помните, что окно браузера не воспринимает объекты как тип данных JavaScript. 
+// Для отображения неоходимо преобразовать строку в ...)
 
-function calculatePowerSum(number) {
-    if (typeof number !== 'number' || number <= 0) {
-        throw new Error('Input should be a positive number');
-    }
-    const digits = number.toString().split('').map(Number); // Разбиваем число на цифры
-    let sum = 0;
+const objectjs = document.querySelector('.object');
 
-    for (let i = 0; i < digits.length; i++) {
-        sum += digits[i] ** (i + 1); // Возводим цифру в степень индекса и добавляем к сумме
-    }
+const obj = {}; 
 
-    return sum;
+for (let i = 1; i < 5; i++) {
+    obj[i] = `объект${i}`;
 }
-const inputNumber = 12345;
 
-try {
-    const result = calculatePowerSum(inputNumber);
-    console.log(`Sum of powers for ${inputNumber} is ${result}`);
-} catch (error) {
-    console.error(error.message);
-}
+const str = JSON.stringify(obj);
+objectjs.textContent = str;
+
+
