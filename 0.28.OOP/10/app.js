4a4668c5e741880511298793a737d0a9c285b725
dariiadrozd <darriiadrozd@gmail.com>
2023-08-28 06:00:40 +0200

    start

--- 0.28.OOP/10/app.js
+++ 0.28.OOP/10/app.js
@@ -1,34 +1,23 @@
-// Реализуйте класс Anagram (Анаграмма это слово или фраза , 
-//   полученный путем замены букв исходного слова или фразы). 
-// Создать функцию для вывода ряда true, если слова являются анаграммами. 
-// Добавить проверки на ввод
+// Дополнить класс Validator. Добавить метод isDate для проверки на дату.
 
-class Anagram {
-  isValid(str1, str2) {
-    if (typeof str1 !== 'string' || typeof str2 !== 'string') {
+
+class Validator {
+  isValid(str) {
+    if (typeof str !== 'string') {
       throw new Error('Вы передали не строку');
     }
   }
 
-  reverseAnagram(str1, str2) {
+  isDate(str) {
     try {
-      this.isValid(str1, str2);
-
-      const reversedStr1 = str1.toLowerCase().split('').sort().join('');
-      const reversedStr2 = str2.toLowerCase().split('').sort().join('');
-
-      return reversedStr1 === reversedStr2;
+      const date = /^\d{4}-\d{2}-\d{2}$/; 
+      return date.test(str); 
     } catch (error) {
       return false;
     }
   }
 }
 
-const value1 = "обезьянство";
-const value2 = "светобоязнь";
-
-const anagram = new Anagram();
-const isReverseAnagram = anagram.reverseAnagram(value1, value2);
-console.log(isReverseAnagram);
-
-
+const validator = new Validator();
+const resultDate = validator.isDate('2023-08-28');
+console.log(resultDate);
