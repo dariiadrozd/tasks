103b3a35ea3ae3e4ae52a4706d829d2a12b04a13
dariiadrozd <darriiadrozd@gmail.com>
2023-08-30 18:03:23 +0200

    start

--- 0.28.OOP/12/app.js
+++ 0.28.OOP/12/app.js
@@ -1,4 +1,22 @@
 // Реализуйте класс Hashtag. В него передается число n – количество элементов
 // массива. На основании числа формируется динамический массив из n элементов внутри класса. 
 // Создать функцию doHashtag, заполняющую новый массив из элементов, каждое значение которого имеет вид #name
- 
+
+class Hashtag {
+    doHashtag(n) {
+        try {
+            let arr = [];
+            for (let i = 0; i < n; i++) {
+                let element = prompt('введите слово');
+                arr.push('#' + element);
+            }
+            return arr;
+        } catch (error) {
+            return error.message;
+        }
+    }
+}
+
+const hashtag = new Hashtag();
+const result = hashtag.doHashtag(3);
+console.log(result);
