1cde61f101fa0cc39bb79ec38cdd7716310677e9
dariiadrozd <darriiadrozd@gmail.com>
2023-09-03 18:56:49 +0200

    start

--- 0.29.Solid/12/app.js
+++ 0.29.Solid/12/app.js
@@ -1,41 +1,19 @@
-// Реализуйте класс DomHtml, который будет взаимодействовать с DOM по клику на кнопку. 
-// Класс содержит 4 метода: doPlus, doMinus, doMultiply, doDivide
-
-class DomHtml {
-  constructor() {
-    this.doEvent();
-  }
-
-  doEvent() {
-    const btn = document.querySelector('button');
-    btn.addEventListener('click', () => {
-      const inp1 = document.querySelector('.inp1').value;
-      const inp2 = document.querySelector('.inp2').value;
-      this.doPlus(inp1, inp2);
-      this.doMinus(inp1, inp2);
-      this.doMultiply(inp1, inp2);
-      this.doDivide(inp1, inp2);
-    });
-  }
-
-  doPlus(inp1, inp2) {
-    const plus = document.querySelector('.plus');
-    plus.innerHTML = +inp1 + +inp2;
-  }
-
-  doMinus(inp1, inp2) {
-    const minus = document.querySelector('.minus');
-    minus.innerHTML = +inp1 - +inp2;
-  }
-
-  doMultiply(inp1, inp2) {
-    const multiply = document.querySelector('.multiply');
-    multiply.innerHTML = +inp1 * +inp2;
-  }
-  doDivide(inp1, inp2) {
-    const divide = document.querySelector('.divide');
-    divide.innerHTML = +inp1 / +inp2
+// Реализуйте класс Sort, сдержащий метод sortData, 
+// сортирующий все числа статичного массива сортировкой пузырьком
+ 
+class Sort {
+  sortData(arr) {
+    for (let i = arr.length; i > 0; i--) {
+      for (let j = 0; j < i; j++) {
+        if (arr[j] > arr[j + 1]) {
+          let sortArr = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = sortArr;
+        }
+      }
+    }
+    return arr;
   }
 }
-
-const domHtml = new DomHtml();
+let sort = new Sort();
+console.log(sort.sortData([1,5,2,7,3,8,4,9,6]));
