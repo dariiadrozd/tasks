b93efac2d3cbeedfb720e4eaf14b1029283a4a79
dariiadrozd <darriiadrozd@gmail.com>
2023-09-03 16:21:59 +0200

    start

--- 0.29.Solid/9/app.js
+++ 0.29.Solid/9/app.js
@@ -1,41 +1,19 @@
-// Реализуйте класс DomHtml, который будет взаимодействовать с DOM по клику на кнопку. 
-// Класс содержит 4 метода: doPlus, doMinus, doMultiply, doDivide
-
-class DomHtml {
-  constructor() {
-    this.doEvent();
-  }
-
-  doEvent() {
-    const btn = document.querySelector('button');
-    btn.addEventListener('click', () => {
-      const inp1 = document.querySelector('.inp1').value;
-      const inp2 = document.querySelector('.inp2').value;
-      this.doPlus(inp1, inp2);
-      this.doMinus(inp1, inp2);
-      this.doMultiply(inp1, inp2);
-      this.doDivide(inp1, inp2);
-    });
-  }
-
-  doPlus(inp1, inp2) {
-    const plus = document.querySelector('.plus');
-    plus.innerHTML = +inp1 + +inp2;
-  }
-
-  doMinus(inp1, inp2) {
-    const minus = document.querySelector('.minus');
-    minus.innerHTML = +inp1 - +inp2;
-  }
-
-  doMultiply(inp1, inp2) {
-    const multiply = document.querySelector('.multiply');
-    multiply.innerHTML = +inp1 * +inp2;
-  }
-  doDivide(inp1, inp2) {
-    const divide = document.querySelector('.divide');
-    divide.innerHTML = +inp1 / +inp2
+// Реализуйте класс Сonversion, сдержащий метод do DecimalNumber, 
+// преобразующий все числа 10 системы счисления в 2
+class Сonversion {
+  DecimalNumber(input1) {
+    const binaryNumber = parseInt(input1).toString(2);
+    return binaryNumber;
   }
 }
+const conversion = new Сonversion();
 
-const domHtml = new DomHtml();
+const btn = document.querySelector('button');
+const input = document.querySelector('input');
+const resultDiv = document.querySelector('div');
+
+btn.addEventListener('click', function () {
+  const input1 = input.value;
+  const result = conversion.DecimalNumber(input1);
+  resultDiv.textContent = `${result}`;
+});
