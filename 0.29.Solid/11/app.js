c4306943d87595e7cd0796fb3f5aced28cd143a3
dariiadrozd <darriiadrozd@gmail.com>
2023-09-03 18:31:19 +0200

    start

--- 0.29.Solid/11/app.js
+++ 0.29.Solid/11/app.js
@@ -1,6 +1,16 @@
-const div = document.querySelector('div');
+// РеализуйтеклассСonversion,сдержащийметодdoOctalNumber,
+// преобразующий все числа 10 системы счисления в 8
 
-div.addEventListener('mouseover', function(event){
-  const res = document.querySelector('.res');
-  res.innerHTML = event.target.textContent;
-})
+class Сonversion{
+
+  doOctalNumber(){
+    const num = parseInt(binary,2);
+    const num2 = num.toString(8);
+    return num2;
+  }
+}
+
+const conversion = new Сonversion();
+const binary = '101010'
+const result = conversion.doOctalNumber();
+console.log(result);
