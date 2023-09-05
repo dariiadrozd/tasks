31ea6691ba43c7b22faa2cf465b3dbb7fcea8cba
dariiadrozd <darriiadrozd@gmail.com>
2023-09-05 20:26:04 +0200

    start

--- 0.31.Polymorphism/3/app.js
+++ 0.31.Polymorphism/3/app.js
@@ -4,3 +4,27 @@
 // родительскому NumberArray и содержит функцию sumArr. 
 // Необходимо переопределить sumArr из базового класса записав в переменную 
 // используя метод super. Далее посчитать сумму всех элементов массива
+
+class NumberArray {
+  sumArr() {
+    const arr = [];
+    for (let i = 0; i < 5; i++) {
+      arr.push(Math.floor(Math.random() * 10))
+    }
+    return arr;
+  }
+}
+
+class ConsoleArray extends NumberArray {
+  sumArr() {
+    const new_arr = super.sumArr();
+    console.log(new_arr);
+    const summa = new_arr.reduce(function (sum, el) {
+      return sum + el
+    }, 0)
+    console.log(summa);
+  }
+}
+
+const consoleArray = new ConsoleArray();
+consoleArray.sumArr()
