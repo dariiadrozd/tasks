ffecac456da2dc79dbfc094c83c442d428a3db3d
dariiadrozd <darriiadrozd@gmail.com>
2023-09-12 20:50:36 +0200

    start

--- 0.32.OOP_2/6/app.js
+++ 0.32.OOP_2/6/app.js
@@ -1,60 +1,17 @@
-// Классы Employee и ProductionWorker.
-// Напишите класс Employee (Сотрудник), который содержит:
-// • имя сотрудника;
-// • номер сотрудника.
-// Затем напишите класс ProductionWorker (Рабочий), который является подклассом класса Employee.
-// Класс ProductionWorker содержит:
-// • номер смены (целое число, к примеру, 1, 2 или 3);
-// • ставка почасовой оплаты труда.
-// Напишите методы получатели и методы-модификаторы для каждого класса. 
-// После того как эти классы будут написаны, напишите программу, которая создает объект класса ProductionWorker и предлагает пользователю ввести данные по каждому атрибуту данных этого объекта. 
-// Сохраните данные в объекте и примените в этом объекте методы получатели, чтобы получить эти данные и вывести их на экран.
 
-class Employee {
-  nameOfEmployee;
-  numOfEmployee;
-  getNameOfEmployee() {
-    return this.nameOfEmployee;
-  }
-  getNumEmployee() {
-    return this.numOfEmployee
-  }
+const new_Obj = {
+  name: 'test'
+}
 
-  setNameOfEmployee() {
-    this.nameOfEmployee = this.nameOfEmployee
-  }
 
-  setNumEmployee(numOfEmployee) {
-    this.numOfEmployee = numOfEmployee
+
+const obj = {
+  id: 1,
+  key: function (a,b) {
+    console.log(this.name);//test
   }
 }
 
-class ProductionWorker extends Employee {
-numOfChange;
-rateOfChange;
-getNumOfChange(){
-  return this.hbaseif 
-}
 
-getRateOfChange(){
-return this.rateOfChange
-}
-
-setNumOfChange(numOfChange){
-this.numOfChange = numOfChange
-}
-
-setRateOfChange(rateOfChange){
-this.rateOfChange = rateOfChange
-}
-}
-
-const productionWorker = new ProductionWorker();
-productionWorker.setNameOfEmployee('Darya');
-productionWorker.setNumEmployee(1);
-productionWorker.setNumOfChange(2);
-productionWorker.setRateOfChange(100);
-console.log(productionWorker.getNameOfEmployee());
-console.log(productionWorker.getNumEmployee());
-console.log(productionWorker.getNumOfChange());
-console.log(productionWorker.getRateOfChange());
+const b = obj.key.bind(new_Obj)
+b()
