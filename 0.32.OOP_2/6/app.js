d4ec4490daae6467523d0737c4edeeff1c7acfd6
dariiadrozd <darriiadrozd@gmail.com>
2023-09-15 21:24:34 +0200

    start

--- 0.32.OOP_2/6/app.js
+++ 0.32.OOP_2/6/app.js
@@ -1,7 +1,6 @@
-const arr = [1, 2, 3, 4, 5,];
-
-const num = arr.reduce(function (el, number) {
-  return el * number;
-},1);
-
-console.log((num));
+class Animal {
+  constructor (name) {
+  this.name = name;
+  }
+  }
+  
