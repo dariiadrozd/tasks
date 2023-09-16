16138785afa014fe51630242fe5b852379b0d4fe
dariiadrozd <darriiadrozd@gmail.com>
2023-09-16 20:49:01 +0200

    startr

--- 0.32.OOP_2/6/app.js
+++ 0.32.OOP_2/6/app.js
@@ -1,6 +1,9 @@
-class Animal {
-  constructor (name) {
-  this.name = name;
+class Person {
+  constructor (username) {
+  this.username = username;
   }
-  }
-  
+}
+  tHe1100 
+  console.log(this .username + '3' + ' Hello");
+  const Jerry = new Person('Jerry");
+  jerry.t.ello ();
