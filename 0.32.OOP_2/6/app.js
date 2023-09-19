7c45f6641482d1fb012f20fa885426edd149e90c
dariiadrozd <darriiadrozd@gmail.com>
2023-09-19 20:55:01 +0200

    start

--- 0.32.OOP_2/6/app.js
+++ 0.32.OOP_2/6/app.js
@@ -1,9 +1,22 @@
-class Person {
-  constructor (username) {
-  this.username = username;
+class MathCalculation {
+  constructor(n) {
+    if (typeof n !== 'number' || n <= 0) {
+      throw new Error('Введите положительное число');
+    }
+    
+    this.array = new Array(n).fill(0).map(() => Math.floor(Math.random() * 100)); // Создаем массив из n элементов со случайными числами
+  }
+
+  countEvenNumbers() {
+    return this.array.filter(number => number % 2 === 0).length;
   }
 }
-  tHe1100 
-  console.log(this .username + '3' + ' Hello");
-  const Jerry = new Person('Jerry");
-  jerry.t.ello ();
+
+try {
+  const n = parseInt(prompt('Введите количество элементов массива:'));
+  const mathCalc = new MathCalculation(n);
+  const evenCount = mathCalc.countEvenNumbers();
+  console.log(`Количество четных чисел: ${evenCount}`);
+} catch (error) {
+  console.error(error.message);
+}
