868de94a3e05364bee770def9de1f0c68a60a8d9
dariiadrozd <darriiadrozd@gmail.com>
2023-10-08 11:35:42 +0200

    start

--- 0.34.Repeat_2/3/app.js
+++ 0.34.Repeat_2/3/app.js
@@ -1,2 +1,24 @@
-// 5. Реализуйте класс Client, содержащий метод doRegistration. Ваша задача получить данные из 2 инпутов: почта, пароль. По клику на кнопку «отправить запрос на сервер» для последующей записи в массив репозитория класса Server
+// Вы вводите числа в поле для ввода формируя и отображая массив.
+// Необходимо также отображать только уникальные значения массива
+// [1, 1, 4, 2, 3, 3] -> [4, 2]
 
+const btn = document.querySelector('button');
+const array = [];
+
+btn.addEventListener('click', () => {
+    const arr = document.querySelector('.arr');
+    const uniq = document.querySelector('.uniq');
+    const inp = document.querySelector('input');
+
+    array.push(inp.value);
+    arr.innerHTML = array;
+    inp.value = '';
+
+    const uniqArr = [];
+    for (let i = 0; i < array.length; i++) {
+        if (!uniqArr.includes(array[i])) {
+            uniqArr.push(array[i]);
+        }
+    }
+    uniq.innerHTML = uniqArr;
+})
