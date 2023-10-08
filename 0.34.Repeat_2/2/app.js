0e0a641469eb65a7dfb278f934f8beac32eafdf9
dariiadrozd <darriiadrozd@gmail.com>
2023-10-08 11:17:36 +0200

    start

--- 0.34.Repeat_2/2/app.js
+++ 0.34.Repeat_2/2/app.js
@@ -1,2 +1,25 @@
-// 5. Реализуйте класс Client, содержащий метод doRegistration. Ваша задача получить данные из 2 инпутов: почта, пароль. По клику на кнопку «отправить запрос на сервер» для последующей записи в массив репозитория класса Server
+// Вы вводите числа в поле для ввода формируя массив.
+// Необходимо отобразить массив, а также отдельно
+// все четные элементы этого массива и нечетные
 
+const btn = document.querySelector('button');
+const array = [];
+
+btn.addEventListener('click', () => {
+    const inp = document.querySelector('input').value;
+    const arr = document.querySelector('.arr');
+    const chet = document.querySelector('.chet');
+    const nechet = document.querySelector('.nechet')
+
+  
+    array.push(inp);
+    arr.innerHTML = array;
+    inp = '';
+
+    const chetArray = [];
+    const nechetArray = [];
+
+    array.forEach((el) => el % 2 == 0 ? chetArray.push(el) : nechetArray.push(el));
+    chet.innerHTML = `четные значения ${chetArray}`;
+    nechet.innerHTML = `нечетные значения ${nechetArray}`
+})
