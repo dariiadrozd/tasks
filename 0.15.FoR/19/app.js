4df20cffc56a35a8d100909a99c1d31e5805e9a5
dariiadrozd <darriiadrozd@gmail.com>
2023-12-01 09:46:57 +0100

    start

--- 0.15.FoR/19/app.js
+++ 0.15.FoR/19/app.js
@@ -1,1 +1,9 @@
 // На входе 5 значений. Все эти значения пользователь вводит с клавиатуры и заполяет массив. Далее необходимо создать массив только из чисел. Если длина массива равна 0, то вывести ‘Массив пуст’. filter
+const inputArray = [userInput1, userInput2, userInput3, userInput4, userInput5];
+const numericArray = inputArray.filter(value => !isNaN(value) && value !== null && value !== '');
+
+if (numericArray.length === 0) {
+  console.log('Массив пуст');
+} else {
+  console.log('Массив чисел:', numericArray);
+}
